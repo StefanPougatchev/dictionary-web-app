49b40911cc4bbda0481e0cb75e072d67ddf79fef
Stefan.Pougatchev <stefan.pougatchev@gmail.com>
2023-12-12 15:44:20 +0200

    my dictionary web app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/dictionary-web-app/', // replace 'your-repo-name' with the name of your repository
+  build: {
+    outDir: 'docs', // GitHub Pages can serve from the 'docs' folder on the master branch
+  },
+});
